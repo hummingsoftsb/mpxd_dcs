0a48b1d38e3cc0e4aca2f2a93f344fce5248211b
aegisrunestone <aegisrunestone3@hotmail.com>
2015-10-05 19:52:14 +0800

    Forgot to change smtp back to google

--- application/libraries/swiftmailer.php
+++ application/libraries/swiftmailer.php
@@ -2,10 +2,10 @@
 
 class Swiftmailer {
 
-	var	$smtp_host		= "dryosaurus.mschosting.com";		// SMTP Server.  Example: mail.earthlink.net
-	var	$smtp_user		= "test1@hummingsoft.com.my";		// SMTP Username
-	var	$smtp_pass		= "humming123";		// SMTP Password
-	var	$smtp_port		= "587";		// SMTP Port
+	var	$smtp_host		= "aspmx.l.google.com";		// SMTP Server.  Example: mail.earthlink.net
+	var	$smtp_user		= "dcs@mymrt.com.my";		// SMTP Username
+	var	$smtp_pass		= "";		// SMTP Password
+	var	$smtp_port		= "25";		// SMTP Port
 	var $mpxd_logo 		= "img/logo_1.png";
 	var $sender_name 	= "MPXD DCS";
 	
@@ -19,8 +19,8 @@ class Swiftmailer {
 	function send($message){ 
 		//Create the Transport 
 		$transport = Swift_SmtpTransport::newInstance ($this->smtp_host, $this->smtp_port) 
-		->setUsername($this->smtp_user)
-		->setPassword($this->smtp_pass); 
+		->setUsername($this->smtp_user);
+		//->setPassword($this->smtp_pass); 
 
 		$mailer = Swift_Mailer::newInstance($transport); 
 
