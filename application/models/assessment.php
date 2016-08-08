6108092a454b96898dcb62243863696f83feaaf3
ancymathew <ancy@hummingsoft.com.my>
2016-08-08 15:26:48 +0530

    Signed-off-by: ancymathew <ancy@hummingsoft.com.my>

--- application/models/assessment.php
+++ application/models/assessment.php
@@ -1227,6 +1227,13 @@ Class Assessment extends CI_Model
     function get_chk_count($pid)
     {
         $query = "select count(jdep.pict_file_name) from journal_data_entry_picture jdep,journal_data_entry_master jdem, journal_master jm, project_template pt, frequency_detail fd where fd.frequency_detail_no = jdem.frequency_detail_no and jdep.data_entry_no = jdem.data_entry_no and jdem.journal_no = jm.journal_no and jm.project_no = pt.project_no and pt.project_no in ($pid)";
+       // echo $query;
+        $q = $this->db->query($query);
+        return $q->result();
+    }
+    function get_image_by_date_one($pdate, $pid)
+    {
+        $query = "select count(jdep.pict_file_name) from journal_data_entry_picture jdep, journal_data_entry_master jdem, journal_master jm, project_template pt, frequency_detail fd where fd.frequency_detail_no = jdem.frequency_detail_no and jdep.data_entry_no = jdem.data_entry_no and jdem.journal_no = jm.journal_no and jm.project_no = pt.project_no and fd.end_date = '{$pdate}' and pt.project_no in ({$pid})";
         //echo $query;
         $q = $this->db->query($query);
         return $q->result();
